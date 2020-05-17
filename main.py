c579a3edd9fc388712d787146cb8fa91345957dd
Pablo Núñez <pablonete@gmail.com>
2020-05-17 10:17:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+width, height = 1000, 1000
+screen = pygame.display.set_mode((height, width))
+
+bg = 135, 182, 189
+ln = 255, 255, 25
+screen.fill(bg)
+
+nxC, nyC = 25, 25
+dimCW = width / nxC   # Ancho de casilla
+dimCH = height / nyC  # Alto de casilla
+
+while True:
+  for y in range(0, nxC):
+    for x in range(0, nyC):
+      posX = x * dimCW
+      posY = y * dimCH
+
+      poly = [(posX,         posY),
+              (posX + dimCW, posY),
+              (posX + dimCW, posY + dimCH),
+              (posX,         posY + dimCH)]
+
+      pygame.draw.polygon(screen, ln, poly, 1)
+
+  pygame.display.flip()
